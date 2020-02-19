1768b8b33cbcfff34df5661d0b45e392f3f376c9
ryuuu1234 <pharyyady@gmail.com>
2020-02-20 00:21:45 +0700

    ubah custom css untuk template admin (1)

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -2,15 +2,23 @@
     <div id="layoutSidenav_nav">
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
+                <div class="sb-header-sidenav-menu">
+                    <button v-on:click="hideSidebar" class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" >
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <span>INI LOGO</span>
+                </div>
                 <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Core</div>
-                    <router-link :to="{name:'dashboard'}" class="nav-link" exact>
-                        <div class="sb-nav-link-icon">
-                            <i class="fas fa-tachometer-alt"></i>
-                        </div>
-                        Dashboard
-                    </router-link>
-
+                    <div class="nav-link-ku">
+                        <router-link :to="{name:'dashboard'}" class="nav-link" exact>
+                            <div class="sb-nav-link-icon">
+                                <i class="fas fa-tachometer-alt"></i>
+                            </div>
+                            Dashboard
+                        </router-link>
+                    </div>
+                   
+                    <div class="nav-link-ku">
                     <a class="nav-link collapsed"  href="#" data-toggle="collapse" data-target="#collapseLayouts"
                         aria-expanded="false"
                         aria-controls="collapseLayouts" >
@@ -18,11 +26,12 @@
                         Layouts
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
+                    </div>
                     <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion" >
                         <nav class="sb-sidenav-menu-nested nav">
-
+                            <div class="nav-link-ku">
                             <router-link :to="{name:'categories'}" class="nav-link" exact>Categories</router-link>
-                            
+                            </div>
                         </nav>
                     </div>
                     
@@ -36,3 +45,21 @@
         </nav>
     </div>
 </template>
+
+<script>
+  import * as auth from '../services/auth_service';
+  export default {
+    methods: {
+      logout: async function(){
+        auth.logout();
+        this.$router.push('/login');
+      },
+
+      hideSidebar: function(e) {
+        e.preventDefault();
+        document.querySelector("body").classList.toggle("sb-sidenav-toggled");
+      }
+    },
+
+  }
+</script>
